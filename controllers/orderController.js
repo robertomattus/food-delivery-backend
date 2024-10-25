bf576749663d36036a76f55f250b7ecdab9fdb96
robert <robertmatt113@gmail.com>
2024-10-24 23:27:57 -0600

    feat: verifyOrder function to check if orders have been paid

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -54,4 +54,21 @@ const placeOrder = async (req, res) => {
   }
 };
 
-export { placeOrder };
+// verify ir order has been paid
+const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
+  try {
+    if (success == "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Not Paid" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder };
