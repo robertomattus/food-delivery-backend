406c121465c57e2b30efdc6f86db1c479ed3c3bf
robert <robertmatt113@gmail.com>
2024-11-21 22:40:32 -0600

    feat: deploy production

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/api/cart", cartRouter);
 app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
-  res.send("API Working");
+  res.send("API funcionando");
 });
 
 app.listen(port, () => {
