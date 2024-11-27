9899777134e13ed9b37ef779d2aea76d62214c92
robert <robertmatt113@gmail.com>
2024-11-26 22:31:35 -0600

    feat: update front url

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // placing user order for frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = process.env.URL_FRONT_SECRET;
+  const frontend_url = "https://eder-front.vercel.app/";
 
   try {
     const newOrder = new orderModel({
@@ -44,8 +44,8 @@ const placeOrder = async (req, res) => {
     const session = await stripe.checkout.sessions.create({
       line_items: line_items,
       mode: "payment",
-      success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-      cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+      success_url: `https://eder-front.vercel.app/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `https://eder-front.vercel.app/verify?success=false&orderId=${newOrder._id}`,
     });
 
     res.json({ success: true, session_url: session.url });
